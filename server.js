37ba75a91be695e4cc356d3acd8c258eed38afc3
SamheinDM <SamheinDM@yandex.ru>
2021-01-25 15:54:39 +0300

    feat: work on connecting database

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+let usersDB, chatsDB, messagesDB;
+
+MongoClient.connect('mongodb://127.0.0.1:27017', function(err, db) {
+  if (err) {throw err}
+  db.createCollection('users');
+  db.createCollection('chats');
+  db.createCollection('messages');
 });
 
 io.on('connection', (socket) => {
   socket.on('requestInfo', (user) => {
     console.log('connect');
+    socket.emit('recieveInfo', 'data');
   });
+
+  socket.on('message', (message) => {
+    const newMessage = {
+      user: message.user,
+      message: message.message,
+      time: new Date().getTime(),
+    }
+    messagesDB.insertOne(newMessage);
+    socket.emit('message', newMessage);
+  })
 });
 
 http.listen(3000, () => {
